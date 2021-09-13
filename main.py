4c9e832f5c05db00b3634e92052ef52a7cab77cb
Alisson <goo.alisson@gmail.com>
2021-09-13 08:42:29 -0300

    'main.py'

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import requests
+import bs4
+
+class Scrappy:
+	def __init__(self) -> None:
+		pass
+
+	def get_url(self, url: str):
+		return requests.get(url).content
+		pass
+
+	def get_content(self, contents):
+		return bs4.BeautifulSoup(contents, "html.parser")
+		pass
+
+scr = Scrappy()
+print(scr.get_content(scr.get_url("https://www.google.com")))
